803b41f1288cb96f9ef232c0c0eabdfa66b8db8f
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-09 05:17:25 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -2,4 +2,4 @@ skill_completed = "Python Syntax"
 exercises_completed = 13
 points_per_exercise = 5
 points_total = 100
-points_total += exercises_completed * po
+points_total += exercises_completed * points_per_exercise
