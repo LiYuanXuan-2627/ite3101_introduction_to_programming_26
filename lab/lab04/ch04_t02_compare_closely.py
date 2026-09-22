54bc6379174317247c42a421d468a69193478b89
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-22 02:12:39 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -12,7 +12,7 @@ bool_three = 19 <= 19
 bool_three = True
 print(boo;bool_three)
 # Set this to True if -22 >= -18 or to False if it is not.
-bool_four = -22 >=
+bool_four = -22 >= -18
 bool_four = None
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
