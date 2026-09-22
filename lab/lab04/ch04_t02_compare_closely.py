97bf16382fab5a9897d72f01c71544c37e086b6a
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-22 02:10:55 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -8,7 +8,7 @@ bool_two = 100 ==(2*50)
 bool_two = True
 print(bool_two)
 # Set this to True if 19 <= 19 or to False if it is not.
-bool_three = 19
+bool_three = 19 <= 19
 bool_three = None
 
 # Set this to True if -22 >= -18 or to False if it is not.
