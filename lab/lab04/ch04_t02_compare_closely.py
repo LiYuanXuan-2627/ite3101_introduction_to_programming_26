610756be9155d19e920dacdf5e977f620086e527
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-22 02:13:10 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -14,7 +14,7 @@ print(boo;bool_three)
 # Set this to True if -22 >= -18 or to False if it is not.
 bool_four = -22 >= -18
 bool_four = False
-print
+print(bool_four)
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
 bool_five = None
