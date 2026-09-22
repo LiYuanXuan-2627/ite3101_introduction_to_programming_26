f1e266245293aaf5f544114e282c4a6e5b2d6ccb
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-22 02:22:40 +0000

    [skip actions] lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -2,9 +2,10 @@
 
 # Make me true!
 bool_one = 3 < 5  # We already did this one for you!
-bool_one = R
+bool_one = True
 
 # Make me false!
+bool_two = 50 != 5 ** 2
 bool_two = None
 
 # Make me true!
