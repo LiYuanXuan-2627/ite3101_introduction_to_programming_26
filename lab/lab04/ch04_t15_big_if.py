d52b56424b432540a9399797106df582457e0f5b
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-22 03:05:09 +0000

    [skip actions] lab/lab04/ch04_t15_big_if.py

--- lab/lab04/ch04_t15_big_if.py
+++ lab/lab04/ch04_t15_big_if.py
@@ -2,7 +2,7 @@
 def grade_converter(grade: int) -> str:
     if grade >= 90:
         return "A"
-    elif grade >= 80:
+    elif grade >= 80 and :
         return "B"
     elif grade >= 70:
         return "C"
