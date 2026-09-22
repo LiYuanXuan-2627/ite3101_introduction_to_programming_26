d3eb9981f2948e15fc315de43b694d6a19d4a79d
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-22 02:16:58 +0000

    [skip actions] lab/lab04/ch04_t03_compare_closelier.py

--- lab/lab04/ch04_t03_compare_closelier.py
+++ lab/lab04/ch04_t03_compare_closelier.py
@@ -6,6 +6,7 @@ bool_one = False  # We did this one for you!
 
 # (10 + 17) == 3**16
 # Remember that ** can be read as 'to the power of'. 3**16 is about 43 million.
+bool_two = 
 bool_two = None
 
 # 1**2 <= -1
