83a749d0bbc13249cabe51cd3b7ff4353917256e
LiYuanXuan-2627 <yinchake2207@gmail.com>
2026-09-23 02:50:50 +0000

    [skip actions] lab/lab05/ch05_t09_ending_up.py

--- lab/lab05/ch05_t09_ending_up.py
+++ lab/lab05/ch05_t09_ending_up.py
@@ -6,6 +6,6 @@ if len(original) > 0 and original.isalpha():
     word = original.lower()
     first = word[0]
     new_word = word + first + pyg
-    print
+    print[1:len(new_word)]
 else:
     print('empty')
